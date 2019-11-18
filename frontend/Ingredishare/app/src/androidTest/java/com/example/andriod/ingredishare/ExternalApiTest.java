ccb7b88a562228431372c061e818e421cfd9b5c9
DESKTOP-ODQNC7K\Brandon <bflholmes@gmail.com>
2019-11-18 00:00:55 -0800

    Tests

---
+++ frontend/Ingredishare/app/src/androidTest/java/com/example/andriod/ingredishare/ExternalApiTest.java
@@ -0,0 +1,56 @@
+package com.example.andriod.ingredishare;
+
+import com.example.andriod.ingredishare.Main.MainActivity;
+import com.facebook.FacebookActivity;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.times;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExternalApiTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testFacebookActivityLaunched() throws InterruptedException {
+        Intents.init();
+        onView(withId(R.id.fb_login_button)).perform(click());
+        intended(hasComponent(FacebookActivity.class.getName()), times(1));
+        UiDevice.getInstance(getInstrumentation()).pressBack();
+        Intents.release();
+    }
+
+    // Has to be the last test or it will break
+    @Test
+    public void testGoogleActivityLaunched() throws InterruptedException {
+        Intents.init();
+        onView(withId(R.id.google_sign_in_button)).perform(click());
+        intended(hasPackage("com.google.android.gms"));
+        Intents.release();
+        UiDevice.getInstance(getInstrumentation()).pressBack();
+    }
+}
---
+++ frontend/Ingredishare/app/src/androidTest/java/com/example/andriod/ingredishare/NotificationTest.java
@@ -0,0 +1,55 @@
+package com.example.andriod.ingredishare;
+
+import com.example.andriod.ingredishare.IngredientList.IngredientListActivity;
+import com.example.andriod.ingredishare.NewIngrediPost.NewIngrediPostActivity;
+import com.facebook.FacebookActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.uiautomator.UiDevice;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.times;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class NotificationTest {
+
+    @Rule
+    public ActivityTestRule<NewIngrediPostActivity> activityRule
+            = new ActivityTestRule<>(NewIngrediPostActivity.class);
+
+    @Test
+    public void testPostWorks() {
+        onView(withId(R.id.name)).perform(typeText("name"), closeSoftKeyboard());
+        onView(withId(R.id.description)).perform(typeText("description"), closeSoftKeyboard());
+        assert (activityRule.getActivity().isFinishing());
+        UiDevice.getInstance(getInstrumentation()).pressBack();
+    }
+
+    @Test
+    public void testPostWorksWithEmptyName() {
+        onView(withId(R.id.description)).perform(typeText("description"), closeSoftKeyboard());
+        assert (activityRule.getActivity().isFinishing());
+        UiDevice.getInstance(getInstrumentation()).pressBack();
+    }
+
+    @Test
+    public void testPostWorksWithEmptyDescription() {
+        onView(withId(R.id.name)).perform(typeText("name"), closeSoftKeyboard());
+        assert (activityRule.getActivity().isFinishing());
+        UiDevice.getInstance(getInstrumentation()).pressBack();
+    }
+}
