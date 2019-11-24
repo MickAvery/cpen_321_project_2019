bc5747c390d444cc4a2e8ac18ffb815bfe273e8b
taravirginillo <taravirginillo@gmail.com>
2019-11-24 14:35:03 -0800

    bug fixes

--- frontend/Ingredishare/app/src/main/java/com/example/andriod/ingredishare/IngredientList/IngredientListPresenter.java
+++ frontend/Ingredishare/app/src/main/java/com/example/andriod/ingredishare/IngredientList/IngredientListPresenter.java
@@ -18,6 +18,7 @@ import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import com.example.andriod.ingredishare.DataManager;
 import com.example.andriod.ingredishare.event.Event;
@@ -38,6 +39,7 @@ public class IngredientListPresenter {
     private EventAdapter eventAdapter;
     private RecyclerView mRecycler;
     private ImageView mNotificationImage;
+    private static final Integer dateMultiplier = 24 * 60 * 60 * 1000;
 
     public IngredientListPresenter(DataManager dataManager, IngredientListView view,
                                    EventAdapter eventAdapter, RecyclerView recycler, ImageView notificationImage) {
@@ -99,8 +101,9 @@ public class IngredientListPresenter {
                             String description = json_data.getString(mContext.getString(R.string.description));
                             String userid = "none";
                             String type = "Post";
-                            // Subtracting 10 days from todays date if it doesnt have it
-                            Long date = System.currentTimeMillis() - 10 * 24 * 60 * 60 * 1000;
+
+                            // Hacky ~ any post that doesn't have a date will be given today - 4
+                            Long date = System.currentTimeMillis() - 4*dateMultiplier;
                             if(json_data.has(mContext.getString(R.string.userId))) {
                                 userid = json_data.getString(mContext.getString(R.string.userId));
                             }
@@ -114,14 +117,12 @@ public class IngredientListPresenter {
                             }
                             Double x = Double.parseDouble(json_data.getString("lat"));
                             Double y = Double.parseDouble(json_data.getString("long"));
-                          //  Double x = 1.0;
-                          //  Double y = 1.0;
 
                             Date today = new Date(System.currentTimeMillis());
                             Date postDate = new Date(date);
 
                             int diffInDays = (int)( (today.getTime() - postDate.getTime())
-                                    / (1000 * 60 * 60 * 24) );
+                                    / dateMultiplier );
                             if(diffInDays < MyApplication.getExpirationDate()) {
                                 Event event = new Event(userid, name, description, x, y, type, date);
                                 eventAdapter.addEvent(event);
