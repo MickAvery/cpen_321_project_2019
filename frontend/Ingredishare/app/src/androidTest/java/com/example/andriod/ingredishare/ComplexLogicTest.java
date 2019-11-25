c3a50d8e1ce236226cc687a63090bf475675102b
DESKTOP-ODQNC7K\Brandon <bflholmes@gmail.com>
2019-11-24 16:01:47 -0800

    add test stability

--- frontend/Ingredishare/app/src/androidTest/java/com/example/andriod/ingredishare/ComplexLogicTest.java
+++ frontend/Ingredishare/app/src/androidTest/java/com/example/andriod/ingredishare/ComplexLogicTest.java
@@ -3,22 +3,30 @@ package com.example.andriod.ingredishare;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.andriod.ingredishare.Main.MainActivity;
+import com.example.andriod.ingredishare.main.MainActivity;
 
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertEquals;
@@ -33,8 +41,16 @@ public class ComplexLogicTest {
     public ActivityTestRule<MainActivity> activityRule
             = new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void initValidString() throws InterruptedException {
+        onView(withId(R.id.email_edit_text)).perform(typeText("qwert@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.password_edit_text)).perform(typeText("qwerty"), closeSoftKeyboard());
+        onView(withId(R.id.log_in_button)).perform(click());
+    }
+
     @Test
     public void testListIsNotEmpty() {
+        onView(isRoot()).perform(waitFor(3000));
         onView(withId(R.id.recycler_view)).check(new RecyclerViewItemCountAssertion(greaterThan(5)));
     }
 
@@ -59,5 +75,21 @@ public class ComplexLogicTest {
             assertEquals(first.getText(), "Coffee with milk");
         }
 
+    }
+
+    public static ViewAction waitFor(long delay) {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return ViewMatchers.isRoot();
+            }
+
+            @Override public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
     }
 }
